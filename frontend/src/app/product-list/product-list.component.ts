eecef99f197e9c21ea3120dea220d31eab96b540
Dennis A. Boanini <dennis.boanini01@gmail.com>
2020-12-26 02:19:10 +0100

    Created product list module

---
+++ frontend/src/app/product-list/product-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'demo-product-list',
+	templateUrl: './product-list.component.html',
+	styleUrls: ['./product-list.component.scss']
+})
+export class ProductListComponent implements OnInit {
+
+	constructor() {
+	}
+
+	ngOnInit(): void {
+	}
+
+}
